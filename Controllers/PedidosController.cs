1f64851b0c5a5c1b79c16ef1b153c0d55de04d7c
Luiz Cecilio <luiz.cecilio@luizalabs.com>
2024-10-18 00:44:45 -0300

    Correção.

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -1,5 +1,6 @@
 ﻿using LojaManoel.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LojaManoel.Controllers
 {
@@ -20,12 +21,8 @@ namespace LojaManoel.Controllers
         private List<ResultViewModel> Encaixotar(PedidoViewModel pedidos)
         {
             List<ResultViewModel> result = new List<ResultViewModel>();
+
             
-
-
-
-
-            int i = 0;
             foreach(var pedido in pedidos.pedidos)
             {
                 ResultViewModel caixaResult = new ResultViewModel();
@@ -79,11 +76,124 @@ namespace LojaManoel.Controllers
                 {
                     if(produtos.Count > 2)
                     {
-                        produtos = new List<string>();
-                        foreach (var produtossepara in produtos)
+                        List<string> produtos2 = new List<string>();
+                        List<string> produtos1 = new List<string>();
+                        int largura1 = 0;
+                        int altura1 = 0;
+                        int comprimento1 = 0;
+                        int largura2 = 0;
+                        int altura2 = 0;
+                        int comprimento2 = 0;
+                        int area1 = 0;
+                        int area2 = 0;
+                        
+
+                        for (int j = 0; j <2; j++)
+                        {
+                            produtos1.Add(produtos[j]);
+                        }
+                        for(int i = 2; i < 4; i++)
+                        {
+                            produtos2.Add(produtos[i]);
+                        }
+
+
+                        int p = 0;
+                        List<string> produtosNovo1 = new List<string>();
+                        List<string> produtosNovo2 = new List<string>();
+                        List<int> areas = new List<int>();
+
+                        foreach (var produtopedido in pedido.produtos)
                         {
 
+                            if (produtos1[p] == produtopedido.produto_id)
+                            {
+                                produtosNovo1.Add(produtopedido.produto_id);
+                                largura1 = largura1 + produtopedido.dimensoes.largura;
+                                altura1 = altura1 + produtopedido.dimensoes.altura;
+                                comprimento1 = comprimento1 + produtopedido.dimensoes.comprimento;
+
+                            }
+                            else
+                            {
+                                produtosNovo2.Add(produtopedido.produto_id);
+                                largura2 = largura2 + produtopedido.dimensoes.largura;
+                                altura2 = altura2 + produtopedido.dimensoes.altura;
+                                comprimento2 = comprimento2 + produtopedido.dimensoes.comprimento;
+                            }
+                            if (p < 1)
+                            {
+                                p = p + 1;
+                            }                            
                         }
+
+                        area1 = altura1 * comprimento1 * largura1;
+                        area2 = altura2 * comprimento2 * largura2;
+                        areas.Add(area1);
+                        areas.Add(area2);
+                        for (int n = 0; n <= 1; n++)
+                        {
+                            caixa = new Caixa();
+                            
+                            if (areas[n] < 160000)
+                            {
+                                caixa.caixa_id = "Caixa 1";
+                                if (n == 0)
+                                {
+                                    caixa.produtos = produtosNovo1;
+                                }
+                                else
+                                {
+                                    caixa.produtos = produtosNovo2;
+                                }                                
+                                                                
+                            }
+                            else if (areas[n] >= 160000 && areas[n] < 240000)
+                            {
+                                
+                                caixa.caixa_id = "Caixa 2";
+                                if (n == 0)
+                                {
+                                    caixa.produtos = produtosNovo1;
+                                }
+                                else
+                                {
+                                    caixa.produtos = produtosNovo2;
+                                }
+                                
+                            }
+                            else if (areas[n] == 240000)
+                            {                                
+                                caixa.caixa_id = "Caixa 3";
+                                if (n == 0)
+                                {
+                                    caixa.produtos = produtosNovo1;
+                                }
+                                else
+                                {
+                                    caixa.produtos = produtosNovo2;
+                                }                                
+                                
+                            }
+                            else
+                            {
+                                caixa.caixa_id = null;
+                                if (n == 0)
+                                {
+                                    caixa.produtos = produtosNovo1;
+                                }
+                                else
+                                {
+                                    caixa.produtos = produtosNovo2;
+                                }
+                                caixa.observacao = "Produto não cabe em nenhuma caixa disponível.";
+                                
+                            }
+
+                            listaCaixas.Add(caixa);
+                        }
+                        pedidoCaixa.pedido_id = pedido.pedido_id;
+                        pedidoCaixa.caixas = listaCaixas;
 
                     }
                     else
@@ -99,7 +209,7 @@ namespace LojaManoel.Controllers
                 }
                 
 
-                i = i + 1;
+                
 
                 listaPedidoCaixa.Add(pedidoCaixa);
                 
